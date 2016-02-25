c1a509a066d4e435aa3efcd6386818120e94abe5
Steven-Kha <stevenvkha@gmail.com>
2016-02-25 14:44:18 -0800

    Finished Assign 2

---
+++ solutions--Steven-Kha/main.cpp
@@ -0,0 +1,6 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Steven Kha <stevenvkha@gmail.com>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+Worked with Ryan Allen, Arshi Singh, and Alex Chau.
